bfd9adb6c04432b6e25335755f6eb93af42647a7
Deeraj <bdeeraj082@gmail.com>
2025-05-10 15:22:50 +0530

    normal changes

--- Quiz_app/Quiz_app_frontend/TopicSelectionPage/startPage1.js
+++ Quiz_app/Quiz_app_frontend/TopicSelectionPage/startPage1.js
@@ -1,23 +1,21 @@
-if (window.localStorage.getItem('totalcrt') == 0) {
-    window.localStorage.setItem('totalqns', 0);
-    window.localStorage.setItem('totalcrt', 0);
-} else {
-    let tq = parseInt(window.localStorage.getItem('totalqns'));
-    let tc = parseInt(window.localStorage.getItem('totalcrt'));
-    let percentage = (tq == 0) ? 0 : Math.floor((tc * 100) / tq);
-if(tc!==NaN){
 
-    document.querySelector('.totalqns').innerHTML =(tq!=NaN)?`${tq}`:'0';
-    document.querySelector('.totalqnscrt').innerHTML = (tc!==NaN)?`${tc}`:'0';
-    document.querySelector('.totalqnswarg').innerHTML = (percentage!=NaN)?`${percentage}`:'0';
+if(window.localStorage.getItem('totalcrt')==0){
+    window.localStorage.setItem('totalqns',0);
+    window.localStorage.setItem('totalcrt',0);
 }
+else{
+    let tq=parseInt(window.localStorage.getItem('totalqns'));
+    let tc=parseInt(window.localStorage.getItem('totalcrt'));
+    document.querySelector('.totalqns').innerHTML=`${tq}` ;
+    document.querySelector('.totalqnscrt').innerHTML=`${tc}`;
+    document.querySelector('.totalqnswarg').innerHTML=`${(tc==0)?0:Math.floor(((tc*100)/tq))}%`
 }
 
-function menuHider() {
-    document.querySelector('.menu_list').classList.toggle('activate');
+function menuHider(){
+document.querySelector('.menu_list').classList.toggle('activate');
 }
-
-function infoFetcher(topic) {
-    window.localStorage.setItem("topic", topic);
-    window.location.href = '../QuizBeginPage/Quiz_entry_page.html';
-}
+function infoFetcher(topic){
+        window.localStorage.setItem("topic",topic)
+        console.log(window.localStorage.getItem('topic'));
+        window.location.href='Quiz_entry_page.html'
+       }
