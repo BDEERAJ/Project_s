b9d689218f4c73b04dafd12485075061e5353689
Deeraj <bdeeraj082@gmail.com>
2025-07-29 21:04:17 +0530

    few changes have been made

--- Leave_req_app/frontend/src/App.jsx
+++ Leave_req_app/frontend/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import  StudentSigin from './components/StudentSignup.jsx';
 import HomePage from './components/HomePage.jsx';
 import StudentLogin from './components/StudentLogin.jsx';
-import TeachersignUp from './components/Teachersignup.jsx'
-import Teacherlogin from './components/Teacherlogin.jsx'
+import Teachersignup from './components/teachersignup.jsx'
+import Teacherlogin from './components/teacherlogin.jsx'
 import TeacherDashboard from './components/TeacherDashboard.jsx'
 import Studentdashboad from './components/StudentDashboard.jsx'
 import {Route,BrowserRouter as Router,Routes} from 'react-router-dom';
@@ -15,7 +15,7 @@ return(
 <Route path='/Student/sign-up' element={<StudentSigin/>}/>
 <Route path='/Student/login' element={<StudentLogin/>}/>
 <Route path='/Teacher/login' element={<Teacherlogin/>}/>
-<Route path='/Teacher/Signup' element={<TeachersignUp/>}/>
+<Route path='/Teacher/Signup' element={<Teachersignup/>}/>
 <Route path='/Teacher/dashboard' element={<TeacherDashboard/>}/>
 <Route path='/student/dashboard' element={<Studentdashboad/>}/>
 
--- Leave_req_app/frontend/src/components/teachersignup.jsx
+++ Leave_req_app/frontend/src/components/teachersignup.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/AuthPage.css'; // Assuming you have a shared style for auth pages
+import '../styles/AuthPage.css'; 
 
 export default function TeacherSignup() {
     const navigate = useNavigate();
